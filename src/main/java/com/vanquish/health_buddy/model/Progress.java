2386385a0a6cdcb34ab7b462a09d6d7c46ed11e9
Annie Kwon <annie.s.kwon@gmail.com>
2022-07-07 15:38:29 -0400

    added progress model

---
+++ src/main/java/com/vanquish/health_buddy/model/Progress.java
@@ -0,0 +1,142 @@
+package com.vanquish.health_buddy.model;
+
+import javax.persistence.*;
+
+@Entity(name = "progress")
+public class Progress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer progressId;
+
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private Double height;
+    @Column(nullable = false)
+    private Double weight;
+    @Column(nullable = false)
+    private Double waist;
+    @Column(nullable = false)
+    private Double neck;
+    @Column(nullable = false)
+    private Double hip;
+
+
+    @Enumerated
+    @Column(nullable = false)
+    private UnitOfMeasurement unitOfMeasurement;
+
+    @Enumerated
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Enumerated
+    @Column(nullable = false)
+    private ActivityLevel activityLevel;
+
+    @Enumerated
+    @Column(nullable = false)
+    private WeightGoal weightGoal;
+
+    public Integer getProgressId() {
+        return progressId;
+    }
+
+    public Progress setProgressId(Integer progressId) {
+        this.progressId = progressId;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Progress setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Progress setHeight(Double height) {
+        this.height = height;
+        return this;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public Progress setWeight(Double weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public Double getWaist() {
+        return waist;
+    }
+
+    public Progress setWaist(Double waist) {
+        this.waist = waist;
+        return this;
+    }
+
+    public Double getNeck() {
+        return neck;
+    }
+
+    public Progress setNeck(Double neck) {
+        this.neck = neck;
+        return this;
+    }
+
+    public Double getHip() {
+        return hip;
+    }
+
+    public Progress setHip(Double hip) {
+        this.hip = hip;
+        return this;
+    }
+
+    public UnitOfMeasurement getUnitOfMeasurement() {
+        return unitOfMeasurement;
+    }
+
+    public Progress setUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) {
+        this.unitOfMeasurement = unitOfMeasurement;
+        return this;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Progress setGender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public ActivityLevel getActivityLevel() {
+        return activityLevel;
+    }
+
+    public Progress setActivityLevel(ActivityLevel activityLevel) {
+        this.activityLevel = activityLevel;
+        return this;
+    }
+
+    public WeightGoal getWeightGoal() {
+        return weightGoal;
+    }
+
+    public Progress setWeightGoal(WeightGoal weightGoal) {
+        this.weightGoal = weightGoal;
+        return this;
+    }
+}
