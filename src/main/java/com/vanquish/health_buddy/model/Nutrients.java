f6fb3ad080bab1de2c9fd849adad13b3b61090f6
Anthonie Etienne <anthonieetienneii@gmail.com>
2022-07-07 08:12:30 -0700

    Added Nutrients Model & Repo

--- src/main/java/com/vanquish/health_buddy/model/Nutrients.java
+++ src/main/java/com/vanquish/health_buddy/model/Nutrients.java
@@ -1,2 +1,75 @@
-package com.vanquish.health_buddy.model;public class Nutrients {
+package com.vanquish.health_buddy.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "nutrients")
+public class Nutrients {
+
+    @Id
+    private Integer userId;
+
+    @Column(nullable = false)
+    private Integer protein;
+
+    @Column(nullable = false)
+    private Integer calories;
+
+    @Column(nullable = false)
+    private Integer fat;
+
+    @Column(nullable = false)
+    private Integer water;
+
+    @Column(nullable = false)
+    private Integer carbs;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getProtein() {
+        return protein;
+    }
+
+    public void setProtein(Integer protein) {
+        this.protein = protein;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public Integer getFat() {
+        return fat;
+    }
+
+    public void setFat(Integer fat) {
+        this.fat = fat;
+    }
+
+    public Integer getWater() {
+        return water;
+    }
+
+    public void setWater(Integer water) {
+        this.water = water;
+    }
+
+    public Integer getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(Integer carbs) {
+        this.carbs = carbs;
+    }
 }
--- src/main/java/com/vanquish/health_buddy/repository/NutrientsRepository.java
+++ src/main/java/com/vanquish/health_buddy/repository/NutrientsRepository.java
@@ -1,2 +1,9 @@
-package com.vanquish.health_buddy.repository;public interface NutrientsRepository {
+package com.vanquish.health_buddy.repository;
+
+import com.vanquish.health_buddy.model.Nutrients;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NutrientsRepository extends JpaRepository<Nutrients, Integer> {
 }
