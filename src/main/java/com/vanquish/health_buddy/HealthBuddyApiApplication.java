4e5d6297c13fefeb8cf66d19e2999f47ee2ffe72
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-06 19:22:03 -0700

    initial commit

---
+++ src/main/java/com/vanquish/health_buddy/HealthBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.vanquish.health_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBuddyApiApplication.class, args);
+	}
+
+}
