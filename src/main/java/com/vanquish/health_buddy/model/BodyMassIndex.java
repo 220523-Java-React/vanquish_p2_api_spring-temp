1039987b186b3751e191254857fac59f213d95fa
Phat Huynh <Phathuynhcoding@gmail.com>
2022-07-07 15:34:02 -0500

    made enums for BFP,BMI,WTHEIGHT,WTHIP

---
+++ src/main/java/com/vanquish/health_buddy/model/BodyMassIndex.java
@@ -0,0 +1,12 @@
+package com.vanquish.health_buddy.model;
+
+public enum BodyMassIndex {
+    VERY_SEVERELY_UNDERWEIGHT,
+    SEVERELY_UNDERWEIGHT,
+    UNDERWEIGHT,
+    NORMAL,
+    OVERWEIGHT,
+    MODERATELY_OBESE,
+    SEVERELY_OBESE,
+    VERY_SEVERELY_OBESE
+}
