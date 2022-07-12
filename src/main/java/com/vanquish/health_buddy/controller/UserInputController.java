2e86b7b692e07c1d57d7e0cdfaf240e3b07d9d61
Phat Huynh <Phathuynhcoding@gmail.com>
2022-07-11 22:07:55 -0500

    created userinput service and controller

---
+++ src/main/java/com/vanquish/health_buddy/controller/UserInputController.java
@@ -0,0 +1,34 @@
+package com.vanquish.health_buddy.controller;
+
+import com.vanquish.health_buddy.model.userInput.UserInput;
+import com.vanquish.health_buddy.service.UserInputService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/userinputs")
+public class UserInputController {
+    private final UserInputService userInputService;
+
+    public UserInputController(UserInputService userInputService){
+        this.userInputService = userInputService;
+    }
+
+    @GetMapping
+    public List<UserInput> getAllUserInputs(){return userInputService.getAllUserInput();}
+
+    @PostMapping
+    public UserInput createUserInput(@RequestBody UserInput userInput){
+        return userInputService.createUserInput(userInput);
+    }
+
+    @GetMapping("/id/{id}")
+    public UserInput getUserInputById(@PathVariable Integer userInputId){
+        return userInputService.getUserInputById(userInputId);
+    }
+
+
+
+
+}
